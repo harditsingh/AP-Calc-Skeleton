8836b4a88858ef53d0111d814a3ec43681fc504d
Hardit Singh <hardit@outlook.com>
2017-09-08 18:55:02 +0200

    Not much, just added some comments

--- src/main/java/CalculatorResources.java
+++ src/main/java/CalculatorResources.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
-public class Tokenizer {
+public class CalculatorResources {
+	/*
+	 * Here we create the methods for parsing, handling shunting yard, rpn and calculating the output
+	 * */
+	
     static final String OPERATOR_TOKENS = "+-*/^";
 
     public TokenList readTokens(String input) {
